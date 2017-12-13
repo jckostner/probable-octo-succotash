dfd10fe58f14f53e04bcca762d8316dac32a2800
jckostner94 <jckostner94@k-state.edu>
2017-12-13 11:02:18 -0600

    Moved SQL scripts to scripts folder

--- countries.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO counties(countryID, name) VALUES
-(1, "United States),
-(2, "Australia"),
-(3, "Canada"),
-(4, "Costa Rica"),
-(5, "France"),
-(6, "Japan"),
-(7, "Mexico"),
-(8, "United Kingdom");
---
+++ scripts/genres data.sql
@@ -0,0 +1,16 @@
+INSERT INTO genres(genreID, genre) VALUES 
+(1,"Documentary"),
+(2,"Romance"),
+(3,"War"),
+(4,"Horror"),
+(5,"Science Fiction"),
+(6,"Western"),
+(7,"Thriller"),
+(8,"Comedy"),
+(9, "Animation"),
+(10, "Sports"),
+(11, "Musical"),
+(12, "Mystery"),
+(13, "Indie"),
+(14, "Drama"),
+(15, "Action");
